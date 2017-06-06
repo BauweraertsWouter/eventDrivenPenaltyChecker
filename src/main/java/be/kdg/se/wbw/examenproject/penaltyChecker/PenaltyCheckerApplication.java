db7a4fe268118535d4d2a4acb19a42f4827aa1b9
BauweraertsWouter <wouter.bauweraerts@telenet.be>
2017-06-06 18:32:37 +0200

    initial commit

---
+++ src/main/java/be/kdg/se/wbw/examenproject/penaltyChecker/PenaltyCheckerApplication.java
@@ -0,0 +1,12 @@
+package be.kdg.se.wbw.examenproject.penaltyChecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenaltyCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenaltyCheckerApplication.class, args);
+	}
+}
---
+++ src/test/java/be/kdg/se/wbw/examenproject/penaltyChecker/PenaltyCheckerApplicationTests.java
@@ -0,0 +1,16 @@
+package be.kdg.se.wbw.examenproject.penaltyChecker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PenaltyCheckerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
